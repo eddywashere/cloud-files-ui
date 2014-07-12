5ee353cf826cf3cc4187efac93dfbe21f145ac90
Eddy Hernandez <eddy.hernandez@rackspace.com>
2014-07-12 14:06:15 -0700

    init commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cloudFilesUiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the cloudFilesUiApp
+ */
+angular.module('cloudFilesUiApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cloudFilesUiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cloudFilesUiApp
+ */
+angular.module('cloudFilesUiApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
